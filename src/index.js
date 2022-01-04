3a0da8782832bb2677ade41fa0998464e99dff8a
Amanda <amandaben21@aol.com>
2022-01-03 21:15:08 -0500

    Added folders for components, reducers, actions, & containers

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
+import './index.css';   
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension'; //replaces window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+import App from './containers/App'
+import { notesReducer } from './reducers/notesReducer'
+
+import { BrowserRouter as Router} from 'react-router-dom';
+
+const store = createStore( 
+  notesReducer, 
+  composeWithDevTools(applyMiddleware(thunk))
+)
+
+ReactDOM.render( 
+  <Provider store={store}> 
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
+    
   document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+)
